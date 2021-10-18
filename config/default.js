0c42f143fb9f5b2d55e0e55a7c4c873ab6ba9172
Vladimir Cambur <vlad@smartdata.solutions>
2021-10-18 16:44:01 +0000

    app.json is back

--- config/default.js
+++ config/default.js
@@ -1,8 +1,8 @@
 module.exports.development = {
-  username: 'root',
-  password: 'admin123',
-  database: 'database_development',
-  host: '127.0.0.1',
+  username: 'mysql',
+  password: '797edc20398462f6',
+  database: 'internship_nodejs_api',
+  host: 'dokku-mysql-internship-nodejs-api',
   dialect: 'mysql',
 };
 
