a83cbeb6042a386e6b929d922dca3f44ab807393
Alina Pavalachi <alinapavalachi@Alinas-MacBook-Air.local>
2021-10-20 15:46:51 +0300

    edit index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,6 @@ dotenv.config();
 
 const port = process.env.PORT || 8000;
 
-<<<<<<< HEAD
-app.listen( port, () => {
-=======
 app.listen(port, () => {
->>>>>>> d2d09642cd1861abc295dc100113be1ce5bb2088
   console.log(`listening on port ${port}`);
 });
